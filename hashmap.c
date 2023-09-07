9e545ee5e0bf9afe909a876e16bd67d9bf36380d
Replit user <>
2023-09-07 17:12:59 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -111,9 +111,8 @@ Pair *searchMap(HashMap *map, char *key) {
 Pair *firstMap(HashMap *map) {
   Pair *firstPair = NULL;
   for (int i = 0; i < map->capacity; i++) {
-    Pair *hypPair = map->buckets[i];
-    if (hypPair->key != NULL) {
-      firstPair = hypPair;
+    if (map->buckets[i] != NULL && map->buckets[i]->key != NULL) {
+      firstPair = map->buckets[i];
       break;
     }
   }
